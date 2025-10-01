85e95a61bb3a6292b413616317d795a5a4e09433
Francesco <77879644+VampaFrancesco@users.noreply.github.com>
2025-10-01 15:39:35 +0200

    Initialize Weather App User Services project
    Set up a new Spring Boot project for Weather App User Services. Added basic configuration files, main application class, test class, Maven build file, and ignore settings for common IDEs and build artifacts.

---
+++ src/main/java/tech/aesys/finale/user/WeatherAppUserServicesApplication.java
@@ -0,0 +1,13 @@
+package tech.aesys.finale.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppUserServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppUserServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/aesys/finale/user/WeatherAppUserServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.aesys.finale.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppUserServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
